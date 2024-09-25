de57a46ea1151db8792b766355206885c5ad8b0e
Santivr23 <santivilro@gmail.com>
2024-09-25 19:37:09 +0200

    Cambio en el index

--- Cliente/blackjack/js/index.js
+++ Cliente/blackjack/js/index.js
@@ -1,7 +1,6 @@
 import { Game } from './modules/game.js';
 
 let game = new Game();
-
 let nombre = game.player.name;
-
-console.log(nombre);
+let saludo = document.getElementById("saludo");
+saludo.textContent = "Hola " + nombre;
