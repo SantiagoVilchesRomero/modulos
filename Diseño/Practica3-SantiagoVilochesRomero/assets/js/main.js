7b7a489b93527c188cae701354f4f6b6821669d8
Santivr23 <santivilro@gmail.com>
2024-10-26 02:37:40 +0200

    todos los hero

--- Diseño/Practica3-SantiagoVilochesRomero/assets/js/main.js
+++ Diseño/Practica3-SantiagoVilochesRomero/assets/js/main.js
@@ -4,4 +4,12 @@ $(document).ready(function () {  /* Cuando esté el documento HTML totalmente ca
         $(".list-nav").toggleClass("view");  /* Añado la clase .show a .nav-list */
         $(".hamburger svg").toggleClass("invert");
     });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 100) {
+            $('.menu').addClass('sticky');
+        } else {
+            $('.menu').removeClass('sticky');
+        }
+    })
 });
