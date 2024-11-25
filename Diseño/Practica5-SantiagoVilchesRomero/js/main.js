870415bf7a017c59dee6bf6c80f94a0db5e29ea9
Santivr23 <santivilro@gmail.com>
2024-11-25 22:53:08 +0100

    Inicio de la practica 5

---
+++ Diseño/Practica5-SantiagoVilchesRomero/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+    $(".menu-toggle").click(() => {
+        $(".list-nav").toggleClass("list-nav-show");
+        $("nav").toggleClass("show");
+        $(".bar:nth-child(1)").toggleClass("rotate-45");
+        $(".bar:nth-child(2)").toggleClass("hide");
+        $(".bar:nth-child(3)").toggleClass("rotate45");
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 100) {
+            $('.menu').addClass('sticky');
+            $('.fa-arrow-up').addClass('change');
+        } else {
+            $('.menu').removeClass('sticky');
+            $('.fa-arrow-up').removeClass('change');
+        }
+    });
+
+    $('.fa-arrow-up').hover(
+        function() {
+            // Cuando el mouse entra
+            $(this).addClass('fa-bounce');
+        },
+        function() {
+            // Cuando el mouse sale
+            $(this).removeClass('fa-bounce');
+        }
+    );
+});
