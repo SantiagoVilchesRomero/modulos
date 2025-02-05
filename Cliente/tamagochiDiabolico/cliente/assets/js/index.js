9b2b156fe3c0c17e13a6c2ae485810656604f70c
Santivr23 <santivilro@gmail.com>
2025-02-05 19:00:54 +0100

    .

--- Cliente/tamagochiDiabolico/cliente/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-import { ConnectionHandler } from "../../src/services/ConnectionHandler.js";
-
-ConnectionHandler.init("http://localhost:3000", () => {
-    console.log("Connected to server");
-}, () => {
-    console.log("Disconnected from server");
-});
-
--- Cliente/tamagochiDiabolico/cliente/src/services/GameService.js
+++ Cliente/tamagochiDiabolico/cliente/src/services/GameService.js
@@ -1,13 +1,62 @@
-export const GameService = {
-    States: {
+import { Board } from "../entities/Board.js";
+import { Queue } from "../Queue.js";
+export class GameService {
+    #states = {
         WAITING: 0,
         PLAYING: 1,
-        FINISHED: 2,
-    },
-    state: GameService.States.WAITING,
-    players: [],
-    board: null,
-    action: (message) => {
-        //Proximamente (en clase hicimos un switch pero da asco)
+        ENDED: 2
+    };
+    #ui = null;
+    #players = [];
+    board = null;
+    #queue = null;
+    #state = null;
+    #parallel = null;
+    #actionsList = {
+        "NEW_PLAYER": this.do_newPlayer,
+        "BOARD": this.do_newBoard
+    };
+    constructor() {
+
+        this.#state = this.#states.WAITING;
+        console.log('Se ejecuta el constructor')
+        this.board = new Board()
+        console.log(this.board);
+        this.#queue = new Queue();
+        this.#parallel = setInterval(async () => {
+            while (true) {
+                const a = this.#queue.getMessage();
+                console.log('A contiene');
+                console.log(a);
+                if (a != undefined) {
+                    console.log('Acabo de sacar');
+                    console.log(a);
+                }
+            }
+        }, 10000);
+        console.log('Ya he terminado el constructor')
+
+        //this.#parallel(this.#queue);
     }
+    set uiSetter(ui) {
+        this.#ui = ui;
+    }
+    do(data) {
+        this.#queue.addMessage(data);
+        console.log('Entra en la cola');
+        console.log(data);
+        //this.#actionsList[data.type] (data.content)
+    };
+    do_newPlayer(payload) {
+        console.log("ha llegado un jugador nuevo");
+    };
+
+    do_newBoard(payload) {
+        console.log('Aqui va #board');
+        console.log(this.board);
+        this.board.build(payload);
+        this.#ui.drawBoard(payload);
+
+    }
+
 }
